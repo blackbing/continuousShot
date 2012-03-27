25124a413c455cbf7f32e89a58c511059353da95
Bingo Yang <blackbing@gmail.com>
2012-03-27 10:27:26 -0400

    add preview

--- javascript/capture.js
+++ javascript/capture.js
@@ -2,30 +2,30 @@ define([
   'javascript/filter.js'
 ],function(Filter){
   var video = {
+    deferred: $.Deferred(),
     videoId : 'video',
     previewSize:{
       width: 197*0.3,
       height: 351*0.3
     },
 
-    capture: function() {
-      var $video = $(this.videoId);
-      var video = $video[0];
+    capture: function(video, size) {
+      var $video = $(video);
+
       var $device = $('#device');
       var scaleFactor =  video.videoWidth/$video.width()
 
       var canvas = document.createElement('canvas');
-      canvas.width  = this.previewSize.width;
-      canvas.height = this.previewSize.height;
+      canvas.width  = size.width;
+      canvas.height = size.height;
       var ctx = canvas.getContext('2d');
-//      var sX = (($video.width()-$device.width())/2 + 22)*scaleFactor;
       var sX = ($('#device').position().left+ 22)*scaleFactor;
       var sY = 29*scaleFactor;
       var sWidth = 197*scaleFactor;
       var sHeight = 351*scaleFactor;
 
       ctx.drawImage(video, sX, sY, sWidth, sHeight,
-          0,0, this.previewSize.width,this.previewSize.height);
+          0,0, canvas.width, canvas.height);
       //ctx.drawImage(video, sX, sY, $device.width(), $device.height());
       return canvas;
     },
@@ -34,20 +34,63 @@ define([
     /**
      * Invokes the <code>capture</code> function and attaches the canvas element to the DOM.
      */
-    shoot: function(){
-        scaleFactor = this.scaleFactor;
+    shoot: function(size){
+        size = size || this.previewSize;
         var video  = $(this.videoId)[0];
+        var currentTime = video.currentTime;
         var output = $('#output');
-        var canvas = this.capture(video, scaleFactor);
+        var canvas = this.capture(video, size);
         $filter = $('input[name="filter"]:checked');
         if($filter.val()){
           Filter[$filter.val()](canvas);
         }
+        $(canvas).data('currentTime', currentTime).prependTo($('#output'));
         //console.log(canvas.toDataURL());
+        //output.prepend(canvas);
+    },
 
-        output.prepend(canvas);
+    preview: function(currentTime){
+      var me = this;
+      $preview = $('.preview');
+      var size = {
+        width:$preview.width(),
+        height: $preview.height()
+      };
+      var bgVideo  = $('#backgroundVideo')[0];
+      bgVideo.currentTime = currentTime;
+      $preview.empty().show();
+
+      //capture while seeked
+      this.deferred = $.Deferred();
+      this.deferred.done(function(){
+        var output = $('#output');
+        var canvas = me.capture(bgVideo, size);
+        $filter = $('input[name="filter"]:checked');
+        if($filter.val()){
+          Filter[$filter.val()](canvas);
+        }
+        $preview.append($(canvas).hide());
+        $(canvas).fadeIn('fast');
+
+        //$('body').append($(canvas));
+      });
     }
   }
+
+  $(document).ready(function(){
+    var $backgroundVideo = $('#video').clone().attr({
+      'loop': false,
+      'id':'backgroundVideo'
+    }).appendTo('#bg');
+    video.backgroundVideo = $backgroundVideo[0];
+    video.backgroundVideo.muted = true;
+    video.backgroundVideo.load();
+    //trigger seeked
+    $backgroundVideo.bind('seeked', function(){
+      if(video.deferred)
+        video.deferred.resolve();
+    });
+  });
   return video;
 
 })
--- javascript/main.js
+++ javascript/main.js
@@ -4,8 +4,9 @@ require([
   'javascript/sound.js'
 ], function(module, video, sound){
 
-
-
+  var flashIt = function(){
+    $('.flash').css('opacity', 0.5).show().animate({opacity:0}, 100);
+  };
   var captureSt = null;
   $('#control').on('click', function(){
 
@@ -14,6 +15,7 @@ require([
       $(this).removeClass().addClass('shot');
     }else if($(this).hasClass('shot')){
       console.log('trigger shot!!')
+      flashIt();
 
       video.shoot();
       sound.cameraShot();
@@ -22,14 +24,28 @@ require([
   }).on('mousedown touchstart', function(){
     if($(this).hasClass('shot')){
       captureSt = setInterval(function(){
+        flashIt();
         video.shoot();
         sound.cameraShot();
       }, 100);
     }
   });
 
+  $('#output canvas').live('click', function(){
+    var currentTime = $(this).data('currentTime');
+    video.preview(currentTime);
+  });
+
+  $('.preview').live('click', function(){
+    $(this).fadeOut('fast');
+  });
+
   $(document).on('mouseup touchend', function(){
     clearInterval(captureSt);
+    /*
+    if($('.preview').is(':visible')){
+      $('.preview').trigger('click');
+    }*/
   })
 
 
